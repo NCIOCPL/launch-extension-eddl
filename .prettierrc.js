6296b0fc2a68b502e06ab2cf9d3d1adf0efff036
Arce, Paul (NIH/NCI) [C] <arcepe@mail.nih.gov>
2020-03-24 13:30:59 -0400

    (#1) Add linting and editor configs

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  proseWrap: 'always',
+  semi: false,
+  singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	jsxBracketSameLine: true
+}
